8d9c15ae4e61242f747a75cc00124d7de086d9fa
bsm10@i.ua <bsm10@i.ua>
2018-09-02 18:07:08 +0300

    NetStandart 1.4

--- Dots/Properties/Resources.Designer.cs
+++ Dots/Properties/Resources.Designer.cs
@@ -32,19 +32,19 @@ namespace DotsGame.Properties {
         internal Resources() {
         }
         
-        /// <summary>
-        ///   Возвращает кэшированный экземпляр ResourceManager, использованный этим классом.
-        /// </summary>
-        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Resources.ResourceManager ResourceManager {
-            get {
-                if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DotsGame.Properties.Resources", typeof(Resources).Assembly);
-                    resourceMan = temp;
-                }
-                return resourceMan;
-            }
-        }
+        ///// <summary>
+        /////   Возвращает кэшированный экземпляр ResourceManager, использованный этим классом.
+        ///// </summary>
+        //[global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        //public static global::System.Resources.ResourceManager ResourceManager {
+        //    get {
+        //        if (object.ReferenceEquals(resourceMan, null)) {
+        //            global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DotsGame.Properties.Resources", typeof(Resources).Assembly);
+        //            resourceMan = temp;
+        //        }
+        //        return resourceMan;
+        //    }
+        //}
         
         /// <summary>
         ///   Перезаписывает свойство CurrentUICulture текущего потока для всех
